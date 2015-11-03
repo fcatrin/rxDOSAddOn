53c90201dbf762d908ff30dda3c5f304ff872878
Franco Catrin <fcatrin@gmail.com>
2015-11-03 23:54:25 +0000

    Agrega immersive mode compatible con pre-kitkat
    git-svn-id: https://svn.xtvapps.com/svn/unica/trunk/retrobox/android/com.fishstix.dbox@3042 fb8f5c45-ab54-496a-8d9e-4c10816ea98d

--- src/xtvapps/retrobox/dosbox/DosBoxLauncher.java
+++ src/xtvapps/retrobox/dosbox/DosBoxLauncher.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 
+import retrobox.utils.ImmersiveModeSetter;
 import retrobox.vinput.GenericGamepad.Analog;
 import retrobox.vinput.Mapper;
 import retrobox.vinput.Mapper.ShortCut;
@@ -261,31 +262,19 @@ public class DosBoxLauncher extends Activity {
 	
 	public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) new Handler().postDelayed(new Runnable(){
-			@Override
-			public void run() {
-				setImmersiveMode();
-			}
-		}, 5000);
+        if (hasFocus) ImmersiveModeSetter.postImmersiveMode(new Handler(), getWindow(), isLayoutStable());
 	}
 
-	@TargetApi(Build.VERSION_CODES.KITKAT)
 	private void setImmersiveMode() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			getWindow().getDecorView().setSystemUiVisibility(
-		            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-		            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-		            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-		            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-		            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-		            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-		} else {
-			
-		}
+		ImmersiveModeSetter.get().setImmersiveMode(getWindow(), isLayoutStable());
 	}
 	
 	private boolean needsOverlay() {
 		return !Mapper.hasGamepads();
+	}
+	
+	private boolean isLayoutStable() {
+		return Mapper.hasGamepads();
 	}
 	
 	private void setupGamepadOverlay(ViewGroup root) {
@@ -362,6 +351,8 @@ public class DosBoxLauncher extends Activity {
         
 
 		super.onResume();
+		ImmersiveModeSetter.postImmersiveMode(new Handler(), getWindow(), isLayoutStable());
+		
 		pauseDosBox(false);
 		
 		DosBoxMenuUtility.loadPreference(this,prefs);
